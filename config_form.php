e719db96139cc7e05be37eb16751dfc20662b5c7
Eric C. Weig <eweig@email.uky.edu>
2019-01-29 13:28:07 -0500

    Create config_form.php

---
+++ config_form.php
@@ -0,0 +1,43 @@
+<?php
+$lang_page_statement = get_option('default_lang_page_statement');
+$view = get_view();
+?>
+
+contributor
+
+coverage
+
+creator
+
+date
+
+description
+
+format
+
+identifier
+
+<div class="field">
+    <h3>Default Language</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
+
+publisher
+
+relation
+
+rights
+
+source
+
+subject
+
+title
+
+type
+
