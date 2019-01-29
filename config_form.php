9b62403478e22af927f09ec8ae2112c790942720
Eric C. Weig <eweig@email.uky.edu>
2019-01-29 15:48:00 -0500

    Update config_form.php

--- config_form.php
+++ config_form.php
@@ -3,20 +3,78 @@ $lang_page_statement = get_option('default_lang_page_statement');
 $view = get_view();
 ?>
 
-contributor
 
-coverage (http://www.getty.edu/research/tools/vocabularies/tgn/index.html)
+<div class="field">
+    <h3>Default Contributor</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
 
-creator
 
-date (https://www.w3.org/TR/NOTE-datetime)
+<div class="field">
+    <h3>Default Coverage</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
 
-description
-
-format (https://www.iana.org/assignments/media-types/media-types.xhtml)
+ (http://www.getty.edu/research/tools/vocabularies/tgn/index.html)
 
 
-identifier
+<div class="field">
+    <h3>Default Creator</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
+
+
+<div class="field">
+    <h3>Default Date</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
+
+(https://www.w3.org/TR/NOTE-datetime)
+
+
+<div class="field">
+    <h3>Default Description</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
+
+
+<div class="field">
+    <h3>Default Format</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
+
+(https://www.iana.org/assignments/media-types/media-types.xhtml)
+
 
 
 https://www.ietf.org/rfc/rfc4646.txt
@@ -30,18 +88,79 @@ https://www.ietf.org/rfc/rfc4646.txt
     </div>
 </div>
 
-publisher
 
-relation
+<div class="field">
+    <h3>Default Publisher</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
 
-rights
+<div class="field">
+    <h3>Default Relation</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
 
-source
 
-subject
 
-title
+<div class="field">
+    <h3>Default Rights</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
 
-type (Collection , Dataset , Event , Image , InteractiveResource , MovingImage , PhysicalObject , Service , Software , Sound , StillImage , Text)
+
+<div class="field">
+    <h3>Default Source</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
+
+<div class="field">
+    <h3>Default Subject</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
+
+<div class="field">
+    <h3>Default Title</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
+
+<div class="field">
+    <h3>Default Type</h3>
+    <div class="inputs">
+        <?php echo $view->formTextarea('lang_page_statement', $lang_page_statement, array('rows' => '1', 'cols' => '30', 'class' => array('textinput'))); ?>
+        <p class="explanation">
+            Add or edit this Language entry.  This will be added to the Dublin Core Language field automatically when a new or exising record is saved, but if and only if that field is empty.
+        </p>
+    </div>
+</div>
+
 http://dublincore.org/documents/dcmi-type-vocabulary/
 
